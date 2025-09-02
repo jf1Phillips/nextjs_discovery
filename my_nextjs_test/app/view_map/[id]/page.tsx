db5915debaf3948b9d7c9ac870a6d2e9e4954057
Tanguy <tanguy.folliot@epitech.eu>
2025-09-02 20:47:47 +0200

    fix: fix the postion of the element and remove the useless hello world

--- my_nextjs_test/app/view_map/[id]/page.tsx
+++ my_nextjs_test/app/view_map/[id]/page.tsx
@@ -10,15 +10,12 @@ export default function MapNbr(
     const params = use(props.params);
     return (
         <>
-            <p className="text-customWhite flex items-center justify-center">
+            <p className="mt-[50px] text-customWhite flex items-center justify-center">
                 Map nbr {params.id}
             </p>
-            <div className="flex items-center justify-center w-full">
+            <div className="mt-[40px] flex items-center justify-center w-full">
                 <MapDisplay x={2.35522} y={48.8566} zoom={10}/>
             </div>
-            <h1 className="text-3xl font-bold underline text-customWhite">
-                Hello world!
-            </h1>
         </>
     )
 }
