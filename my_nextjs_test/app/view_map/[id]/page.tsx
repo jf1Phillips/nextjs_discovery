9614fa96ec5655fcfa3ffc95f6b731a3ce0ab0e1
Tanguy <tanguy.folliot@epitech.eu>
2025-09-02 14:59:11 +0200

    feat: dispmay first mapbox map

--- my_nextjs_test/app/view_map/[id]/page.tsx
+++ my_nextjs_test/app/view_map/[id]/page.tsx
@@ -1,12 +1,34 @@
-export default async function MapNbr(
-    props: {
-        params: Promise<{id: string}>
-    }
-) {
-    const params = await props.params;
+"use client";
+
+import { useEffect, useRef } from "react";
+import mapboxgl, {Map} from "mapbox-gl";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN as string;
+
+export default function MapNbr ({
+    params,
+}: {
+    params: {id: string}
+}) {
+    const mapContainer = useRef<HTMLDivElement | null>(null);
+    const map = useRef<Map | null>(null);
+
+    useEffect(() => {
+        if (map.current) return;
+
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current as HTMLDivElement,
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [2.3522, 48.8566],
+            zoom: 10,
+        });
+
+
+    }, []);
     return (
         <>
             <p>Map nbr {params.id}</p>
+            <div ref={mapContainer}></div>
         </>
     )
 }
