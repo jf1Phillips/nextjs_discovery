0674c63ec89f8032e01c3fba42d4bd2294f14e5d
Tanguy <tanguy.folliot@epitech.eu>
2025-09-02 17:24:10 +0200

    feat: add tailwind rules to center the element

--- my_nextjs_test/app/view_map/[id]/page.tsx
+++ my_nextjs_test/app/view_map/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { use } from "react";
 import MapDisplay from "@/component/map";
-import styles from './map.module.css';
+import "@/styles/globals.css";
 
 export default function MapNbr(
     props: {
@@ -11,7 +11,9 @@ export default function MapNbr(
     return (
         <>
             <p>Map nbr {params.id}</p>
-            <MapDisplay x={2.35522} y={48.8566} zoom={12}/>
+            <div className="flex items-center justify-center w-full">
+                <MapDisplay x={2.35522} y={48.8566} zoom={12}/>
+            </div>
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
