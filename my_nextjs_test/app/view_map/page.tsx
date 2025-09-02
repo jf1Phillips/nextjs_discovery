eb902864b1db2087c4343c42882c2d612493d5df
Tanguy <tanguy.folliot@epitech.eu>
2025-09-02 11:19:12 +0200

    feat: add a view map page to the project

---
+++ my_nextjs_test/app/view_map/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+    <>
+        <p>La carte du monde !!!!</p>
+        <Link href="/view_map/45">Lien</Link>
+    </>
+    )
+}
