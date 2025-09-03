b7e3d63d8d53d93505a06ca0f15778f29f75446f
Tanguy <tanguy.folliot@epitech.eu>
2025-09-03 18:10:19 +0200

    feat: add a function to get the loc of the user

---
+++ script/get_loc.ts
@@ -0,0 +1,31 @@
+import atoi from "./atoi";
+
+type fetch_data = {
+    loc: string;
+}
+
+export default async function get_loc(): Promise<{ lat: number, long: number } | undefined> {
+    try {
+        const response = await fetch('https://ipinfo.io/json');
+        if (!response.ok) {
+            return undefined;
+        }
+        const data: fetch_data = await response.json();
+        const location = data.loc.split(',');
+        const lat: number = atoi(location[0]);
+        const long: number = atoi(location[1]);
+
+        return {lat , long};
+    } catch (error) {
+        return undefined;
+    }
+}
+
+// // Utilisation de la fonction
+// getGeolocationByIp().then(location => {
+//   if (location) {
+//     console.log('Received location:', location);
+//   } else {
+//     console.log('Failed to retrieve geolocation.');
+//   }
+// });
