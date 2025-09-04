ec414a7567c73e984cb41f66d017d058f36d03bd
Tanguy <tanguy.folliot@epitech.eu>
2025-09-04 16:57:53 +0200

    feat: add python file to conv csv file into json file

---
+++ conv_csv_to_json.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+from sys import argv
+import csv
+import json
+
+if len(argv) != 2:
+    exit(84)
+
+file_name = argv[1]
+
+def csv_to_json(file_name):
+    points = []
+    with open(file_name, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if not (row["latitude"] and row["longitude"]):
+                continue
+            point = {
+                "longlat": [float(row["longitude"]), float(row["latitude"])],
+                "link": "None",
+                "name": {}
+            }
+            if row["nameFr"]:
+                point["name"]["fr"] = row["nameFr"]
+            if row["nameEn"]:
+                point["name"]["en"] = row["nameEn"]
+            if row["nameZh"]:
+                point["name"]["zh"] = row["nameZh"]
+            if row["nameIt"]:
+                point["name"]["it"] = row["nameIt"]
+            if row["nameEs"]:
+                point["name"]["es"] = row["nameEs"]
+            points.append(point)
+    return {"points": points}
+
+json_data = csv_to_json(file_name)
+output_file = file_name.split('.')[0] + ".json"
+
+with open(output_file, mode='w', encoding='utf-8') as json_file:
+    json.dump(json_data, json_file, indent=4, ensure_ascii=False)
+
+print(f"Fichier JSON généré : {output_file}")
