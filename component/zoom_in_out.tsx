c3bdde04ed1f8d8211bb662b56681a5216580ff4
Tanguy <tanguy.folliot@epitech.eu>
2025-09-05 16:01:54 +0200

    fix: fix the darkmode color of the zoom in zoom out and set a transtion

--- component/zoom_in_out.tsx
+++ component/zoom_in_out.tsx
@@ -8,12 +8,13 @@ export default function ZoomInOut(
     {enabled, zoom, setZoom}: ZoomInOutArgs)
 {
     return (<>
-        <div className="absolute w-[55px] h-[22px] text-[20px] flex flex-row justify-between mt-[120px] ml-[10px]">
-            <button className={`rounded-[2px] w-[22px] h-[22px] flex items-center justify-center transition-bg duration-300
-                        ${enabled ? "bg-customGrey2" : "bg-whiteMode"}`}
+        <div className={`absolute w-[55px] h-[22px] text-[20px] flex flex-row justify-between mt-[120px] ml-[10px]
+                        ${!enabled ? "text-darkMode" : "text-whiteMode"}`}>
+            <button className={`rounded-[2px] w-[22px] h-[22px] flex items-center justify-center duration-[300ms]
+                        ${enabled ? "bg-darkMode" : "bg-whiteMode"}`}
                         onClick={() => {setZoom(zoom - 1)}}>-</button>
-            <button className={`rounded-[2px] w-[22px] h-[22px] flex items-center justify-center transition-bg duration-300
-                        ${enabled ? "bg-customGrey2" : "bg-whiteMode"}`}
+            <button className={`rounded-[2px] w-[22px] h-[22px] flex items-center justify-center duration-[300ms]
+                        ${enabled ? "bg-darkMode" : "bg-whiteMode"}`}
                 onClick={() => {setZoom(zoom + 1)}}>+</button>
         </div>
     </>)
