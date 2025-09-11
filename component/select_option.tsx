0e0fe3b2f158fa3c1673c01df15aa95de0cced14
Tanguy <tanguy.folliot@epitech.eu>
2025-09-11 17:42:02 +0200

    feat: add a file to put the select element in

---
+++ component/select_option.tsx
@@ -0,0 +1,23 @@
+import "@/styles/globals.css";
+
+interface Args {
+    selected: string;
+    setSelected: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function SelectLang({selected, setSelected}: Args)
+{
+    return (
+        <>
+            <div className="absolute">
+                <label htmlFor="lang_select">💬</label>
+                <select id="lang_select" value={selected} onChange={(e) => {setSelected(e.target.value);}}>
+                    <option value="fr">fr</option>
+                    <option value="en">en</option>
+                    <option value="it">it</option>
+                    <option value="es">es</option>
+                </select>
+            </div>
+        </>
+    )
+}
