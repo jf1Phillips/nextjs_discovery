303daf14bd8120a6498d548adae76589900cf1e6
Tanguy <tanguy.folliot@epitech.eu>
2025-09-16 10:18:31 +0200

    refactor: add the map to the refactor

--- component/get_map.tsx
+++ component/get_map.tsx
@@ -5,9 +5,12 @@ import SelectLang from "@/component/select_option";
 import ZoomInOut from "@/component/zoom_in_out";
 import DarkMode from "@/component/darkmode";
 import MapDisplay from "@/component/map";
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
+import mapboxgl, {Map as MapboxMap} from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 import "@/styles/globals.css";
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN as string;
 
 type MapVar = {
     zoom: number;
@@ -21,14 +24,26 @@ const DEFAULT_VALUE: MapVar = {
     lat: 48.8566,
 };
 
-
 export default function GetMapboxMap (): JSX.Element
 {
     const [state, setState] = useState<MapVar>(DEFAULT_VALUE);
+    const map = useRef<MapboxMap | null>(null);
+    const container = useRef<HTMLDivElement | null>(null);
 
-    return (
-        <>
-            <p>Hello</p>
-        </>
-    )
+    useEffect(() => {
+        if (!map.current) {
+            map.current = new mapboxgl.Map({
+                container: container.current as HTMLDivElement,
+            });
+        }
+    });
+
+    return (<>
+        <div className="mt-[30px] flex items-center justify-center w-full">
+            <div
+                className="overflow-hidden"
+                ref={container}
+                style={{ width: "100%", height: "calc(100vh - 165px)" }}/>
+        </div>
+    </>)
 }
