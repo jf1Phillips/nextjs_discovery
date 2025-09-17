24b4d0e1204f654ce752a4262f6c0e5893134143
Tanguy <tanguy.folliot@epitech.eu>
2025-09-17 10:55:13 +0200

    feat: add two button, next and prev

--- app/view_map/[id]/page.tsx
+++ app/view_map/[id]/page.tsx
@@ -3,18 +3,20 @@
 import React, { use, useState } from "react";
 import "@/styles/globals.css";
 import atoi from "@/script/atoi";
-import DarkMode from "@/component/darkmode";
 
 import GetMapboxMap from "@/component/get_map";
+
 
 function DisplayTxt({enabled}: {enabled: boolean}): React.JSX.Element {
     return (<>
         <div className={`flex-row flex justify-between duration-300
             ${enabled ? "bg-darkMode text-whiteMode" : "bg-whiteMode text-darkMode"}
-            fixed mt-[-98px] w-[100%] h-[100px]`}>
-            <button>{"<"}</button>
-            <p className="">Test</p>
-            <button>{">"}</button>
+            fixed mt-[-98px] w-[100%] h-[100px] p-[10px]`}>
+            <button className={`text-[20px] duration-300 h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
+                ${enabled ? "bg-whiteMode text-darkMode" : "bg-darkMode text-whiteMode"}`}>{"<"}</button>
+            <p className="text-[16px]">Test</p>
+            <button className={`text-[20px] duration-300  h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
+                ${enabled ? "bg-whiteMode text-darkMode" : "bg-darkMode text-whiteMode"}`}>{">"}</button>
         </div>
     </>);
 }
