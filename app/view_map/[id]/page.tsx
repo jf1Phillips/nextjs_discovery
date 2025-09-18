7a00323793bf296fc2c7cc2ff9903f2b65184c4b
Tanguy <tanguy.folliot@epitech.eu>
2025-09-18 18:54:11 +0200

    feat: add a button to put the text up or down (for mobile)

--- app/view_map/[id]/page.tsx
+++ app/view_map/[id]/page.tsx
@@ -11,6 +11,7 @@ function DisplayTxt({enabled, setStateTextNbr}:
 {
     const [displayText, setDisplayText] = useState<string>("start...");
     const [textNbr, setTextNbr] = useState<number>(1);
+    const [up, setUp] = useState<boolean>(false);
 
     const api_req: (nbr: number) => string = (nbr:number) => `https://dilexit-back-1001788493975.europe-west9.run.app/bible/pericopes/${nbr}`;
     const set_text_data: (id: number) => void = (id: number) => {
@@ -33,9 +34,17 @@ function DisplayTxt({enabled, setStateTextNbr}:
         set_text_data(new_nbr);
     };
     return (<>
+        <div className={`absolute w-full flex justify-center h-[50px] items-center duration-300
+                ${up ? "bottom-[200px]" : "bottom-[70px]"}`}>
+            <button className={`w-[30px] h-[30px] text-[15px] rounded-full duration-200
+                flex items-center justify-center hover:mb-[10px]
+                ${enabled ? "bg-whiteModeOp text-darMode" : "bg-darkModeOp text-whiteMode"}`}
+                onClick={() => setUp(!up)}>{up ? "△" : "▽"}</button>
+        </div>
+
         <div className={`flex-row flex justify-between duration-300 items-center rounded-t-[10px]
             ${enabled ? "bg-darkModeOp text-whiteMode" : "bg-whiteModeOp text-darkMode"}
-            fixed mt-[-67px] w-[100%] h-[70] px-[20px]`}>
+            fixed bottom-0 w-full h-[70px] px-[20px]`}>
             <button className={`text-[20px] duration-300 h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
                 ${enabled ? "bg-whiteMode text-darkMode" : "bg-darkMode text-whiteMode"}`}
                 onClick={() => {click_btn(-1)}}
--- component/mapbox_functions/addRain.ts
+++ component/mapbox_functions/addRain.ts
@@ -7,12 +7,12 @@ export default function addRain(map: MapboxMap, remove_rain ?: boolean)
     } else if (!map.getRain()){
         map.setRain({
             density: ['interpolate', ['linear'], ['zoom'],
-                11, 0.0, 13, 0.8],
+                9, 0.0, 13, 0.8],
             intensity: 1.0,
             color: '#a8adbc',
             opacity: 0.7,
             vignette: ['interpolate', ['linear'], ['zoom'],
-                11, 0.0, 13, 0.8],
+                9, 0.0, 13, 0.8],
             'vignette-color': '#464646',
             direction: [0, 80],
             'droplet-size': [2.6, 18.2],
