1792d3b03670542ea83ef81a85cf7ef4574f3d65
Tanguy <tanguy.folliot@epitech.eu>
2025-10-22 11:02:08 +0200

    feat: add a new array for all geo img

--- components/mapbox_functions/add_geoimg.ts
+++ components/mapbox_functions/add_geoimg.ts
@@ -28,6 +28,33 @@ export function addRoads(url_given: string, map: MapBoxMap)
 }
 
 export type Coords = [[number, number], [number, number], [number, number], [number, number]];
+
+type GeoImg =
+    |   {
+            url: string,
+            id: string,
+            type: "image",
+            coord: Coords,
+        }
+    |   {
+            url: string,
+            id: string,
+            type: "raster",
+    };
+
+const coord_new_map: Coords = [
+    [34.120542941238725 + 0.008, 33.46703792406347 + 0.003],
+    [35.7498100593699 + 0.008, 33.46703792406347 + 0.003],
+    [35.7498100593699 + 0.008, 31.10529446421723 - 0.0058],
+    [34.120542941238725 + 0.008, 31.10529446421723 - 0.0058],
+];
+// const GEOMAP_FOLDER: string = "/img/geo_map";
+// const GEOMAP_NAME: string = "geo_map_";
+// const NEWMAP_NAME: string = "new_map.jpg";
+const ID_PEF: string = "pef1880map";
+const ID_HANS: string = "hans1975map";
+export {ID_PEF, ID_HANS};
+
 const default_coord: Coords =
 [
     [33.6791210, 33.6868944 + 0.007], // ╭
@@ -35,6 +62,20 @@ const default_coord: Coords =
     [36.6262031, 31.1730673 + 0.007], //  ╯
     [33.6791210, 31.1730673 + 0.007], // ╰
 ];
+
+const geoImgArray: GeoImg[] = [
+    {
+        url: "/img/geo_map/pef_1880_map.jpg",
+        id: ID_PEF,
+        type: "image",
+        coord: [
+            [34.120542941238725 + 0.008, 33.46703792406347 + 0.003],
+            [35.7498100593699 + 0.008, 33.46703792406347 + 0.003],
+            [35.7498100593699 + 0.008, 31.10529446421723 - 0.0058],
+            [34.120542941238725 + 0.008, 31.10529446421723 - 0.0058],
+        ],
+    }
+]
 
 export default function addGeoImg(url_given: string, map: MapBoxMap, coords ?: Coords)
 {
