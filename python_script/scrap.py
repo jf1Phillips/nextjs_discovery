7989e5d10707f52adbae5d42ef5d8a459e39a9b5
Tanguy <tanguy.folliot@epitech.eu>
2025-11-25 18:22:54 +0100

    feat: add new scrapt file for json

--- python_script/scrap.py
+++ python_script/scrap.py
@@ -1,9 +1,70 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 import re
 
 URL = "https://www.miraclehunter.com/marian_apparitions/approved_apparitions/apparitions_1000-1099.html"
 BASEURL = "https://www.miraclehunter.com/marian_apparitions/approved_apparitions/"
+EXPORT_FILE = "scrapv2.json"
+
+def rmEndLine(text: str) -> str:
+    return re.sub(r"\s+", " ", text).strip()
+
+def extract_description(text, field_names):
+    description_lines = []
+    fields = {f.lower() for f in field_names}
+
+    for line in text.strip().split("\n"):
+        stripped = line.strip()
+        if not stripped:
+            continue
+        lower = stripped.lower()
+        if any(lower.startswith(f) for f in fields):
+            continue
+        description_lines.append(stripped)
+    return " ".join(description_lines).splitlines()
+
+def parseLine(text: str) -> list[str]:
+    data = {
+        "visionary": "",
+        "title": "",
+        "description": "",
+        "feast": "",
+        "commemorated": "",
+        "source": "",
+    }
+
+    # visionary
+    match = re.search(r"visionar(?:y|ies):\s*(.*)", text, re.IGNORECASE)
+    if match:
+        data["visionary"] = rmEndLine(match.group(1).strip())
+
+    # title
+    match = re.search(r"title:\s*(.*)", text, re.IGNORECASE)
+    if match:
+        data["title"] = rmEndLine(match.group(1).strip())
+
+    # feast
+    match = re.search(r"feast:\s*(.*)", text, re.IGNORECASE)
+    if match:
+        data["feast"] = rmEndLine(match.group(1).strip())
+
+    # commemorated
+    match = re.search(r"commemorated:\s*(.*)", text, re.IGNORECASE)
+    if match:
+        data["commemorated"] = rmEndLine(match.group(1).strip())
+
+    # source
+    match = re.search(r"source:\s*(.*)", text, re.IGNORECASE | re.DOTALL)
+    if match:
+        data["source"] = rmEndLine(match.group(1).strip())
+
+    # description
+    fields = ["Visionary:", "Visionaries:", "Title:", "Source:", "Feast:", "Commemorated:"]
+    description = extract_description(text, fields)
+
+    data["description"] = rmEndLine(" ".join(description))
+    return data
 
 def scrape_table(url):
     html = requests.get(url).text
@@ -21,9 +82,14 @@ def scrape_table(url):
     results = []
     for tr in target_table.find_all("tr"):
         tds = tr.find_all("td")
+        links = tr.find_all("a")
         if len(tds) == 3:
-            row = [re.sub(r"\s+", " ", str(td.get_text(strip=False))).strip() for td in tds]
+            row = [td.get_text(strip=False) for td in tds]
             if not ("Contact The Miracle Hunter" in row):
+                for link in links:
+                    href = link.get("href", "")
+                    if href != "":
+                        row.append(BASEURL+href)
                 results.append(row)
     return results[1:]
 
@@ -38,11 +104,21 @@ def get_select(url):
     return result[1:]
 
 if __name__ == "__main__":
-    end_urls = get_select(URL)
+    json_data = []
+    end_urls = get_select(URL)[:-1]
     for end in end_urls:
         print(BASEURL+end)
         data = scrape_table(BASEURL+end)
-        with open("scrap.csv", "a") as file:
-            for row in data:
-                file.write('|'.join(row))
-                file.write("\n")
+        for row in data:
+            parse = parseLine(row[2])
+            newList = {
+                "date": rmEndLine(row[0]),
+                "place": rmEndLine(row[1]),
+                **parse,
+                "links": row[3:]
+            }
+            json_data.append(newList)
+
+
+    with open(EXPORT_FILE, "w", encoding="utf-8") as f:
+        json.dump(json_data, f, ensure_ascii=False, indent=4)
