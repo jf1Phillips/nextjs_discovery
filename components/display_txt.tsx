9d3213c0e793b0769f1cd6e2d250c5a1f1864b10
Tanguy <tanguy.folliot@epitech.eu>
2025-12-05 11:41:12 +0100

    feat: add a useRef to set the first chapter

--- components/display_txt.tsx
+++ components/display_txt.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const BDD_ULR: string = "https://om-backend-315602396963.europe-west9.run.app";
 // const BIBLE_ID: number = 5;
@@ -69,6 +69,13 @@ export default function DisplayTxt({ setStateTextNbr, histdate, setHistDate, dar
         });
         setHistDate(histdate + add);
     };
+
+    const scaleDivRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+        if (!scaleDivRef) return;
+        click_btn(0);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (<>
         <div className={`absolute w-full flex justify-center h-[50px] items-center duration-300
                 ${up ? "bottom-[200px]" : "bottom-[70px]"}`}>
@@ -77,25 +84,25 @@ export default function DisplayTxt({ setStateTextNbr, histdate, setHistDate, dar
                 ${darkMode ? "bg-whiteMode text-darMode" : "bg-bgWhiteMode text-whiteMode"}`}
                 onClick={() => setUp(!up)}>{!up ? "△" : "▽"}</button>
         </div>
-            <div className={`rounded-t-[10px] flex flex-col space-y-5 duration-300  pt-4
-                ${darkMode ? "bg-darkMode text-whiteMode" : "bg-bgDarkMode text-darkMode"}
-                    ${up ? "h-[200px]" : "h-[70px]"}
-                    absolute bottom-0 w-full px-[20px]`}>
-                <div className="flex-row flex justify-between duration-300">
-                    <button className={`text-[20px] duration-300 h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
-                        ${darkMode ? "bg-whiteMode text-darkMode" : "bg-bgWhiteMode text-whiteMode"}`}
-                        onClick={() => { click_btn(-1) }}
-                    >{"<"}</button>
-                    <p className="text-[16px]">Chapitre {displayText}</p>
-                    <button className={`text-[20px] duration-300  h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
-                        ${darkMode ? "bg-whiteMode text-darkMode" : "bg-bgWhiteMode text-whiteMode"}`}
-                        onClick={() => { click_btn(1) }}
-                    >{">"}</button>
-                </div>
-                <p className={`
-                    ${!up && "hidden"}
-                    ml-auto mr-auto`}>{chapterName}</p>
-                <div className="scaleDiv right-[10px] top-[-50px] absolute"></div>
+        <div className={`rounded-t-[10px] flex flex-col space-y-5 duration-300  pt-4
+            ${darkMode ? "bg-darkMode text-whiteMode" : "bg-bgDarkMode text-darkMode"}
+                ${up ? "h-[200px]" : "h-[70px]"}
+                absolute bottom-0 w-full px-[20px]`}>
+            <div className="flex-row flex justify-between duration-300">
+                <button className={`text-[20px] duration-300 h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
+                    ${darkMode ? "bg-whiteMode text-darkMode" : "bg-bgWhiteMode text-whiteMode"}`}
+                    onClick={() => { click_btn(-1) }}
+                >{"<"}</button>
+                <p className="text-[16px]">Chapitre {displayText}</p>
+                <button className={`text-[20px] duration-300  h-[25px] w-[40px] items-center justify-center flex rounded-[5px]
+                    ${darkMode ? "bg-whiteMode text-darkMode" : "bg-bgWhiteMode text-whiteMode"}`}
+                    onClick={() => { click_btn(1) }}
+                >{">"}</button>
             </div>
+            <p className={`
+                ${!up && "hidden"}
+                ml-auto mr-auto`}>{chapterName}</p>
+            <div ref={scaleDivRef} className="scaleDiv right-[10px] top-[-50px] absolute"></div>
+        </div>
     </>);
 }
